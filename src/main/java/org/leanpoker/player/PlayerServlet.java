e6196ceb083096e8e83b91318e9e27ad6b100af0
xonix <xonixx@gmail.com>
2016-07-09 14:08:50 +0300

    fix

--- src/main/java/org/leanpoker/player/PlayerServlet.java
+++ src/main/java/org/leanpoker/player/PlayerServlet.java
@@ -55,6 +55,7 @@ public class PlayerServlet extends HttpServlet {
             outputStream.println(reply);
             outputStream.flush();
         } catch (Exception e) {
+            System.out.println("ERROR: " + e.getMessage());
             logger.error("Unexpected error [action=" + action + "]:", e);
         }
     }
