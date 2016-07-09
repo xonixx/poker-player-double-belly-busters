ea8234015e62ad5343abf222ff26235c6b66182e
dmitry <krasilnikov.dmtr@gmail.com>
2016-07-09 12:13:13 +0300

    add logging support

--- src/main/java/org/leanpoker/player/PlayerServlet.java
+++ src/main/java/org/leanpoker/player/PlayerServlet.java
@@ -2,10 +2,10 @@ package org.leanpoker.player;
 
 import com.doublebellybuster.Util;
 import com.doublebellybuster.model.GameState;
-import com.doublebellybuster.model.IGameState;
 import com.doublebellybuster.strategy.IStrategy;
 import com.doublebellybuster.strategy.PushAllInStrategy;
-import com.google.gson.JsonParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -17,6 +17,8 @@ import java.io.IOException;
 
 @WebServlet("/")
 public class PlayerServlet extends HttpServlet {
+
+    private static final Logger logger = LoggerFactory.getLogger(PlayerServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -30,6 +32,7 @@ public class PlayerServlet extends HttpServlet {
         String gameState = req.getParameter("game_state");
         String reply;
         if ("check".equals(action)) {
+            logger.info("Received check request");
             reply = "OK";
         } else if ("bet_request".equals(action)) {
             reply = Integer.toString(strategy.betRequest(Util.parse(gameState, GameState.class)));
