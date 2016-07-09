74c2b2625cde919da6c420fd4205ccdbe98ca273
sviridenich <sergey.sviridenko@gmail.com>
2016-07-09 16:05:32 +0300

    stub for new strategy

---
+++ src/main/java/com/doublebellybuster/strategy/PreflopRaiseContbetStrategy.java
@@ -0,0 +1,14 @@
+package com.doublebellybuster.strategy;
+
+import com.doublebellybuster.model.IGameState;
+
+/**
+ * Created by sviridenich on 7/9/16.
+ */
+
+public class PreflopRaiseContbetStrategy implements IStrategy {
+    @Override
+    public int betRequest(IGameState gameState) {
+        return 0;
+    }
+}
