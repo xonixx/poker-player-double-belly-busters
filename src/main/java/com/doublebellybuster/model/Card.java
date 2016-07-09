104636d51cff6e1d32b65273d1196c9b2bdc3412
xonix <xonixx@gmail.com>
2016-07-09 16:32:12 +0300

    combinations

--- src/main/java/com/doublebellybuster/model/Card.java
+++ src/main/java/com/doublebellybuster/model/Card.java
@@ -1,8 +1,28 @@
 package com.doublebellybuster.model;
 
-public class Card {
+public class Card implements Comparable<Card> {
+    public static final String Ace = "A";
+    public static final String King = "K";
     private String rank;
     private CardSuit suit;
+
+    public Card() {
+    }
+
+    public Card(String val) {
+        val = val.toLowerCase();
+        rank = val.substring(0, val.length() - 1);
+        suit = CardSuit.byChar(val.charAt(val.length() - 1));
+    }
+
+    public Card(String rank, CardSuit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public boolean isSameRank(Card card) {
+        return rank.equals(card.rank);
+    }
 
     public String getRank() {
         return rank;
@@ -12,8 +32,23 @@ public class Card {
         return suit;
     }
 
+    public boolean sameSuit(Card card) {
+        return getSuit() == card.getSuit();
+    }
+
     @Override
     public String toString() {
         return rank + suit;
+    }
+
+    public static final String POS = "1|2|3|4|5|6|7|8|9|10|J|Q|K|A";
+
+    public int getValue() {
+        return POS.indexOf(rank);
+    }
+
+    @Override
+    public int compareTo(Card o) {
+        return Integer.compare(getValue(), o.getValue());
     }
 }
--- src/test/java/com/doublebellybuster/strategy/Tests1.java
+++ src/test/java/com/doublebellybuster/strategy/Tests1.java
@@ -14,10 +14,27 @@ public class Tests1 {
         List<Card> l1 = Arrays.asList(
                 new Card("10", CardSuit.diamonds),
                 new Card("K", CardSuit.clubs),
-                new Card("7", CardSuit.diamonds),
+                new Card("7", CardSuit.spades),
                 new Card("J", CardSuit.hearts)
         );
 
         System.out.println(PokerLogic.top(l1));
+    }
+    @Test
+    public void test2() {
+        List<Card> l1 = Arrays.asList(
+                new Card("10", CardSuit.diamonds),
+                new Card("K", CardSuit.clubs),
+                new Card("7", CardSuit.spades),
+                new Card("J", CardSuit.hearts)
+        );
+        List<Card> l2 = Arrays.asList(
+                new Card("10d"),
+                new Card("Kc"),
+                new Card("7s"),
+                new Card("Jh")
+        );
+        System.out.println(l1);
+        System.out.println(l2);
     }
 }
