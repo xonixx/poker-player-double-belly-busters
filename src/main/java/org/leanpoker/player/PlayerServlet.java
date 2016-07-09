860c6cf9ff45e22f292c1eec4a97a872f818030f
xonix <xonixx@gmail.com>
2016-07-09 14:21:56 +0300

    fix

--- src/main/java/org/leanpoker/player/PlayerServlet.java
+++ src/main/java/org/leanpoker/player/PlayerServlet.java
@@ -27,7 +27,7 @@ public class PlayerServlet extends HttpServlet {
         resp.getWriter().print("Java player is running");
     }
 
-    private static IStrategy strategy = new ConstantStrategy();
+    private static IStrategy strategy = new ShortStackPushStrategy();
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
